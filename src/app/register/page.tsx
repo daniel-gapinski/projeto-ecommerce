d25338d35b0d43cdd3407020c1ff04e3827030ad
Daniel Augusto Gapinski <daniel.gapinski@hotmail.com>
2025-04-29 08:43:42 -0300

    Adicionando stripe

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,21 +1,19 @@
+"use client";
+
+import { useSearchParams } from 'next/navigation';
 import { SignUp } from "@clerk/nextjs";
 
-interface RegisterProps {
-    searchParams: {
-        redirectUrl: string;
-    }
-}
+export default function Register() {
+  const searchParams = useSearchParams();
+  const redirectUrl = searchParams.get("redirectUrl") || "/";
 
-export default function Register({ searchParams: {redirectUrl } }: RegisterProps) {
-    return (
-        <>
-            <section className="mt-14">
-                <div className="container mx-auto px-4">
-                    <div className="flex justify-center">
-                        <SignUp signInUrl="/login" forceRedirectUrl={redirectUrl} />
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <section className="mt-14">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-center">
+          <SignUp signInUrl="/register" forceRedirectUrl={redirectUrl} />
+        </div>
+      </div>
+    </section>
+  );
 }
